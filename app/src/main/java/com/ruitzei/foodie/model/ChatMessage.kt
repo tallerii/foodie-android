3e18c534568f942bbb9cf47107e43fc17204126f
Manuel Ruiz Echazu <ruitzei@gmail.com>
2019-11-01 00:51:00 -0300

    Implemented firebase chat

---
+++ app/src/main/java/com/ruitzei/foodie/model/ChatMessage.kt
@@ -0,0 +1,8 @@
+package com.ruitzei.foodie.model
+
+import com.google.firebase.database.PropertyName
+
+class ChatMessage (
+    @PropertyName("message") var message: String = "",
+    @PropertyName("senderId") var senderId: String = ""
+)
