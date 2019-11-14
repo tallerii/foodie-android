832df7508be563ce622cc01b9623f1e27a3d4e07
Manuel Ruiz Echazu <ruitzei@gmail.com>
2019-11-13 21:31:42 -0300

    Changed endpoints

--- app/src/main/java/com/ruitzei/foodie/service/ApiCalls.kt
+++ app/src/main/java/com/ruitzei/foodie/service/ApiCalls.kt
@@ -8,38 +8,38 @@ import retrofit2.http.*
  * Created by ruitzei on 3/17/18.
  */
 interface ApiCalls {
-    @GET("clients/")
+    @GET("clients")
     fun users(
             @Query("name") name: String?
     ): Call<List<String>>
 
-    @POST("token-auth/username/")
+    @POST("token-auth/username")
     fun login(
         @Body loginData: LoginPostData
     ): Call<LoginResponse>
 
-    @POST("token-auth/facebook/")
+    @POST("token-auth/facebook")
     fun loginWithFacebook(
         @Body loginData: FacebookLoginPostData
     ): Call<LoginResponse>
 
-    @GET("clients/{id}/")
+    @GET("clients/{id}")
     fun getUserData(
         @Path("id") userId: String
     ): Call<User>
 
-    @GET("clients/self/")
+    @GET("clients/self")
     fun getLoggedInUserData(): Call<User>
 
-    @PATCH("clients/self/")
+    @PATCH("clients/self")
     fun updateLatLong(@Body latLong: LatLong): Call<User>
 
-    @PATCH("clients/self/")
+    @PATCH("clients/self")
     fun updateUserData(@Body userDataUpdatePostData: UserDataUpdatePostData): Call<User>
 
-    @POST("orders/")
+    @POST("orders")
     fun createOrder(@Body order: OrderPostObject): Call<Order>
 
-    @GET("orders/")
+    @GET("orders")
     fun getOrders(): Call<List<Order>>
 }
